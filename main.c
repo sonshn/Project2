4341599abee11eb711ae7a14eb1a150ae9ce61b7
sonshn (Son Seung-Hyun) <55887179+sonshn@users.noreply.github.com>
2021-07-04 23:14:20 +0900

    Update main.c
    Final Version

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm time1, time2;
 
 void filestat1();
 void filestat2();
@@ -39,42 +39,122 @@ void filestat2(){
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    time1 = localtime(&stat1.st_mtime);
+    time1 = *localtime(&stat1.st_mtime); 
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    time2 = localtime(&stat2.st_mtime);
+    time2 = *localtime(&stat2.st_mtime); 
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-	printf("size compare\n");
-    printf("size: %d\n", (int)stat1.st_size);
-	printf("size: %d\n", (int)stat2.st_size);
+
+        printf("size compare\n");
+        if ((int)stat1.st_size > (int)stat2.st_size) {
+                printf("text1 is bigger\n");
+        }
+        else if ((int)stat1.st_size < (int)stat2.st_size) {
+                printf("text2 is bigger\n");
+        }
+        else
+                printf("sizes are equal\n");
 	
-	if((int)stat1.st_size > (int)stat2.st_size){
-		printf("text1 is bigger\n");
-	}
-	else if((int)stat1.st_size == (int)stat2.st_size){
-		printf("sizes are equal\n");
-	}
-	else{
-		printf("text2 is bigger\n");
-	}
+	return;
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+
+        printf("block compare\n");
+        if ((int)stat1.st_blocks > (int)stat2.st_blocks) {
+                printf("text1 is bigger\n");
+        }
+        else if ((int)stat1.st_blocks < (int)stat2.st_blocks) {
+                printf("text2 is bigger\n");
+        }
+        else
+                printf("sizes are equal\n");
+	
+	return;
 }
+
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+   printf("date compare\n");
+   if(time1.tm_year < time2.tm_year)
+   {
+      printf("text1 is early\n");
+   }
+   else if(time1.tm_year > time2.tm_year)
+   {
+      printf("text2 is early\n");
+   }
+   else
+   {
+      if(time1.tm_mon < time2.tm_mon)
+      {
+         printf("text1 is early\n");
+      }
+      else if(time1.tm_mon > time2.tm_mon)
+      {
+         printf("text2 is early\n");
+      }
+      else
+      {
+         if(time1.tm_mday < time2.tm_mday)
+         {
+            printf("text1 is early\n");
+         }
+         else if(time1.tm_mday > time2.tm_mday)
+         {
+            printf("text2 is early\n");
+         }
+         else
+         {
+            printf("same date\n");
+         }
+      }
+   }
+   return;    
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+    printf("time compare\n");
+    if(time1.tm_hour < time2.tm_hour)
+   {
+      printf("text1 is early\n");
+   }
+   else if(time1.tm_hour > time2.tm_hour)
+   {
+      printf("text2 is early\n");
+   }
+   else
+   {
+      if(time1.tm_min < time2.tm_min)
+      {
+         printf("text1 is early\n");
+      }
+      else if(time1.tm_min > time2.tm_min)
+      {
+         printf("text2 is early\n");
+      }
+      else{
+         if(time1.tm_sec < time2.tm_sec)
+         {
+            printf("text1 is early\n");
+         }
+         else if(time1.tm_sec > time2.tm_sec)
+         {
+            printf("text2 is early\n");
+         }
+         else
+         {
+            printf("same time\n");
+         }
+      }
+   }
+   return;
 }
